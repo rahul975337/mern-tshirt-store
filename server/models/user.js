8a0e51d7e2ba2db07b5e89afcecefd7cacb385f2
rahul975337 <tilwani03@gmail.com>
2021-05-04 06:06:21 +0530

    added categories

--- server/models/user.js
+++ server/models/user.js
@@ -1,41 +1,71 @@
 var mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-var userSchema = new Schema({
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
+var userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
     maxlength: 32,
-    trim: true,
+    trim: true
   },
   lastname: {
     type: String,
     maxlength: 32,
-    trim: true,
+    trim: true
   },
   email: {
     type: String,
     trim: true,
     required: true,
-    unique: true,
+    unique: true
   },
   userinfo: {
     type: String,
-    trim: true,
+    trim: true
   },
-  //TODO: come back here
-  password: {
+  encry_password: {
     type: String,
-    trim: true,
+    required: true
   },
   salt: String,
   role: {
     type: Number,
-    default: 0,
+    default: 0
   },
   purchases: {
     type: Array,
-    default: [],
-  },
+    default: []
+  }
 });
+
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.method = {
+  autheticate: function(plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
+
+  securePassword: function(plainpassword) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
